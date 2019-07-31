a9eda65e1ed600fccd470947a610541082755fd8
Patrick Jun <Patrick-Jun@126.com>
2019-07-31 09:46:25 +0800

    add swiper

--- src/views/home/components/header.vue
+++ src/views/home/components/header.vue
@@ -8,7 +8,7 @@
             输入城市/景点/游玩主题
         </div>
         <div class="header-right">
-            城市
+            大连
             <span class="t-font arrow-icon">&#xe64a;</span>
         </div>
     </div>
